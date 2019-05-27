7d83961ce70c4bb8b33d7a70b6ef826fdcca655b
priscila guzman <priscilaelisaguzman34@gmail.com>
2019-05-27 12:30:39 -0300

    Cambios en js

--- AutoPrueba2/AutoPrueba2/wwwroot/js/Lista.js
+++ AutoPrueba2/AutoPrueba2/wwwroot/js/Lista.js
@@ -25,7 +25,42 @@ function initVue() {
                         }
                     });
             },
-            
+            obtenerEdit: function () {
+                $.ajax({
+                    //Cambiar a type: POST si necesario
+                    type: "GET",
+                    // Formato de datos que se espera en la respuesta
+                    dataType: "json",
+                    // URL a la que se enviará la solicitud Ajax
+                    url: "/Vehiculos/Edit",
+                })
+                    .done(function (data) {
+                        vm.$data.vehiculo = data;
+                    })
+                    .fail(function (jqXHR, textStatus, errorThrown) {
+                        if (console && console.log) {
+                            console.log("La solicitud ha fallado: " + textStatus);
+                        }
+                    });
+            },
+            obtenerDetele: function () {
+                $.ajax({
+                    //Cambiar a type: POST si necesario
+                    type: "GET",
+                    // Formato de datos que se espera en la respuesta
+                    dataType: "json",
+                    // URL a la que se enviará la solicitud Ajax
+                    url: "/Vehiculos/Detele",
+                })
+                    .done(function (data) {
+                        vm.$data.vehiculo = data;
+                    })
+                    .fail(function (jqXHR, textStatus, errorThrown) {
+                        if (console && console.log) {
+                            console.log("La solicitud ha fallado: " + textStatus);
+                        }
+                    });
+            },
         }
     })
 };
